21fb848e8f86d200ce55227d1cc88fd9199290a5
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2017-03-23 00:35:56 +0300

    my wellgorithms page now loads all the data it needs

--- wp-content/themes/favorfields/template-parts/page-my-wellgorithms.php
+++ wp-content/themes/favorfields/template-parts/page-my-wellgorithms.php
@@ -17,7 +17,8 @@
 get_header();
 
 $author = get_current_user_id();
-
+$tags = array();
+$weather = array();
 $args = array(
     'post_type' => 'my_wellgorithms',
     'author'    => $author, // use 25 for testing
@@ -25,10 +26,32 @@ $args = array(
 // The Query
 $the_query = new WP_Query( $args );
 ?>
+<script>
+    var tags, weather;
+</script>
 <div class="separator" style="height: 150px; width: 100%"></div>
 <!-- Main -->
 <div id="main">
-    <div class="inner">
+    <form action="<?= get_permalink() ?>">
+        <select name="category-filter" id="category-filter">
+            <option value="0">All</option>
+            <? foreach( get_categories() as $category ) : ?>
+                <option value="<?= $category->cat_ID?>"><?= $category->name?></option>
+            <? endforeach; ?>
+        </select>
+        <select name="tags-filter" id="tags-filter">
+            <option value="0">All</option>
+
+        </select>
+
+        <select name="weather-filter" id="weather-filter">
+            <option value="1">All</option>
+        </select>
+
+        <button type="submit" class="submit-filter">Filter</button>
+    </form>
+
+    <div class="wellgorithms-container">
         <?php
         // The Loop
         if ( $the_query->have_posts() ) {
@@ -40,6 +63,30 @@ $the_query = new WP_Query( $args );
                 $image_src = get_post_meta( $post->ID, 'user_basic_settings_icon' )[0];
                 $related_wellgo_id = get_post_meta( $post->ID, 'user_basic_settings_related_wellgo' )[0];
                 $category = strtolower( get_the_category($related_wellgo_id)[0]->name );
+
+                $current_weather =  get_post_meta( $post->ID, 'user_basic_settings_mood')[0];
+                $current_weather_name = "";
+
+                switch($current_weather) {
+                    case 1: $current_weather_name = "Sunshine"; break;
+                    case 2: $current_weather_name = "Storm"; break;
+                    case 3: $current_weather_name = "Rainbow"; break;
+                    case 4: $current_weather_name = "Rain"; break;
+                    case 5: $current_weather_name = "Partly_sunny"; break;
+                    case 6: $current_weather_name = "Clouds"; break;
+                    default: $current_weather_name = "No weather";
+                }
+
+
+                $weather[] = array(
+                    'id' =>  $current_weather,
+                    'name' =>  $current_weather_name
+                );
+
+                $tags[] = array(
+                    'id' => wp_get_post_tags($related_wellgo_id)[0]->term_taxonomy_id,
+                    'name' => wp_get_post_tags($related_wellgo_id)[0]->name,
+                );
 
                 echo '<div class="wellgorithm ' . $category .'">';
 
@@ -69,5 +116,34 @@ $the_query = new WP_Query( $args );
 
     </div>
 </div>
+<script>
+    tags = <?= json_encode( array_values( array_unique( $tags, SORT_REGULAR ) ) )?>;
+    weather = <?= json_encode( array_values( array_unique( $weather, SORT_REGULAR ) ) )?>;
 
+
+
+    jQuery(document).ready(function ($) {
+
+        // Add to tags
+        $.each(tags, function(key, value) {
+            $('#tags-filter')
+                .append($("<option></option>")
+                    .attr("value",value.id)
+                    .text(value.name));
+        });
+
+
+        // Add to weather
+        $.each(weather, function(key, value) {
+            $('#weather-filter')
+                .append($("<option></option>")
+                    .attr("value",value.id)
+                    .text( value.name.replace('_', ' ') ));
+        });
+
+
+    })
+
+
+</script>
 <?php get_footer(); ?>
