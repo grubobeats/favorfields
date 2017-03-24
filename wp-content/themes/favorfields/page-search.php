d7f682d38edcc4092fca474d695c22940fd7c819
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2017-03-24 17:46:01 +0300

    added second instance of instantsearch with replica index

--- wp-content/themes/favorfields/page-search.php
+++ wp-content/themes/favorfields/page-search.php
@@ -197,6 +197,8 @@ get_header(); ?>
 
         'use strict';
 
+        // Instasearch instance 1
+
         var search = instantsearch({
             appId: 'T2B04QR9B0',
             apiKey: '28985d34dee832a8f54db2e290beaec7',
@@ -207,13 +209,6 @@ get_header(); ?>
             container: '#search-box',
             placeholder: 'I feel...',
             autofocus: false
-        }));
-
-        search.addWidget(instantsearch.widgets.searchBox({
-            container: '#search-box-replica',
-            placeholder: 'I seek...',
-            autofocus: true,
-            poweredBy: true
         }));
 
         var hitTemplate = document.getElementById('movie').innerHTML;
@@ -343,6 +338,152 @@ get_header(); ?>
         }));
 
         search.start();
+
+
+
+        // Instasearch instance 2
+
+        var search_replica = instantsearch({
+            appId: 'T2B04QR9B0',
+            apiKey: '28985d34dee832a8f54db2e290beaec7',
+            indexName: 'test_wellgorithms' // test_wellgorithms
+        });
+
+
+        search_replica.addWidget(instantsearch.widgets.searchBox({
+            container: '#search-box-replica',
+            placeholder: 'I seek...',
+            autofocus: true,
+            poweredBy: true
+        }));
+
+        var hitTemplate = document.getElementById('movie').innerHTML;
+
+        search_replica.addWidget(instantsearch.widgets.hits({
+            container: '#hits',
+            hitsPerPage: 100,
+            templates: {
+                item: hitTemplate
+            },
+            transformData: function transformData(hit) {
+                hit.stars = [];
+                for (var i = 1; i <= 3; ++i) {
+                    hit.stars.push(i <= hit.vp_level);
+                }
+                return hit;
+            }
+        }));
+
+        search_replica.addWidget(instantsearch.widgets.refinementList({
+            container: '#focus',
+            attributeName: 'taxonomies.post_tag',
+            limit: 7,
+            templates: {
+                item: function item(data) {
+                    return "<div class='focus__items'>" + data.name + "</div>";
+                }
+            },
+            cssClasses: {
+                list: "titles__list",
+                // item: "justLoaded"
+            }
+        }));
+
+        search_replica.addWidget(instantsearch.widgets.rangeSlider({
+            container: '#confidence',
+            attributeName: 'vp_confidence',
+            tooltips: {
+                format: function format(v) {
+                    return "";
+                }
+            },
+            min: 0,
+            max: 10,
+            pips: false,
+            templates: {
+                footer: 'Confidence Range'
+            }
+        }));
+
+        search_replica.addWidget(instantsearch.widgets.numericRefinementList({
+            container: '#category',
+            attributeName: 'vp_category',
+            limit: 3,
+            autoHideContainer: false,
+            options: [{ start: 4, end: 4, name: 'Hellgo' }, { start: 3, end: 3, name: 'Wellgo' }, { start: 5, end: 5, name: 'Letgo' }],
+            templates: {
+                item: function item(data) {
+                    var output = "";
+                    if (data.name == 'Hellgo') {
+                        output = "<img src='http://favorfields.wpengine.com/wp-content/uploads/2017/02/hellgo3.png'><span class='hellgo'>Hellgo</span>";
+                    } else if (data.name == 'Letgo') {
+                        output = "<img src='http://favorfields.com/wp-content/uploads/2017/03/letgo1.png'><span class='letgo'>Letgo</span>";
+                    } else if (data.name == 'Wellgo') {
+                        output = "<img src='http://favorfields.wpengine.com/wp-content/uploads/2017/02/wellgo1.png'><span class='wellgo'>Wellgo</span>";
+                    }
+                    return output;
+                }
+            },
+            cssClasses: {
+                item: "wellgo_categories"
+            },
+            sortBy: ['name:desc']
+        }));
+
+        search_replica.addWidget(instantsearch.widgets.numericRefinementList({
+            container: '#level',
+            attributeName: 'vp_level',
+            options: [{ start: 1, end: 1, name: '1' }, { start: 2, end: 2, name: '2' }, { start: 3, end: 3, name: '3' }],
+            autoHideContainer: false,
+            templates: {
+                item: function item(data) {
+                    return " ";
+                },
+                footer: "Level"
+            },
+            cssClasses: {
+                list: "wellgo_levels",
+            }
+        }));
+
+        search_replica.addWidget(instantsearch.widgets.numericRefinementList({
+            container: '#mood',
+            attributeName: 'vp_mood',
+            options: [{ start: 1, end: 1, name: 'Sunshine' }, { start: 5, end: 5, name: 'Partly_sunny' }, { start: 6, end: 6, name: 'Clouds' }, { start: 4, end: 4, name: 'Rain' }, { start: 2, end: 2, name: 'Storm' }, { start: 3, end: 3, name: 'Rainbow' }],
+            autoHideContainer: false,
+            templates: {
+                item: function item(data) {
+                    if (data.name == 'Storm') {
+                        return "<img src='http://favorfields.com/wp-content/uploads/2017/03/w-storm.png'>";
+                    } else if (data.name == 'Sunshine') {
+                        return '<img src="http://favorfields.com/wp-content/uploads/2017/03/w-sunny.png">';
+                    } else if (data.name == 'Rainbow') {
+                        return '<img src="http://favorfields.com/wp-content/uploads/2017/03/w-rainbow.png">';
+                    } else if (data.name == 'Rain') {
+                        return '<img src="http://favorfields.com/wp-content/uploads/2017/03/w-rain.png">';
+                    } else if (data.name == 'Clouds') {
+                        return '<img src="http://favorfields.com/wp-content/uploads/2017/03/w-clouds.png">';
+                    } else if (data.name == 'Partly_sunny') {
+                        return '<img src="http://favorfields.com/wp-content/uploads/2017/03/w-partly-sunny.png">';
+                    } else {
+                        return 'x';
+                    }
+                }
+            },
+            cssClasses: {
+                // item: "justLoaded"
+            }
+        }));
+
+        search_replica.addWidget(instantsearch.widgets.clearAll({
+            container: '#stats',
+            templates: {
+                link: '<i class="fa fa-repeat reload_search" aria-hidden="true"></i>'
+            },
+            autoHideContainer: false
+        }));
+
+        search_replica.start();
 
 
         /**
